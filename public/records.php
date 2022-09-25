7fa46a1f52df6593a9b5f36da6e1ddd0c1695654
rravidas27 <102133541+rravidas27@users.noreply.github.com>
2022-09-25 20:37:40 +0800

    Create records.php

---
+++ public/records.php
@@ -0,0 +1,25 @@
+
+<?php    
+error_reporting(0);
+include ('show_date.php');
+?>
+
+<?php
+
+?>
+	<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="./style.css">
+	</head>
+	<body>
+	<div id="header">
+	<h1>Records can be seen by registered user.</h1>
+    <ul>
+    <p class="login-register-text"> <a href="index.php">Back</a>.</p>
+    <br>
+    <br>
+	</div>
+
+    <?php
+
+?>
