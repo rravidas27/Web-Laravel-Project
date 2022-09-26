014322fe6ea15d7a24d8b824f15e140880a4958b
rravidas27 <102133541+rravidas27@users.noreply.github.com>
2022-09-26 17:21:39 +0800

    Create index.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+
+<?php    
+error_reporting(0);
+include ('show_date.php');
+?>
+
+<?php
+?>
+	<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="./style.css">
+	</head>
+	<body>
+	<div id="header">
+	<h1>CLICK IF YOU WANT TO CONTINUE!</h1>
+    <ul>
+    <br>
+    <br>
+    <li><a href= "login.php">Login</a></li> 
+    <li><a href= "register.php">Register</a></li> 
+    <li><a href= "records.php">Records</a></li> 
+    </ul>
+
+	</div>
+
+    <?php
+?>
